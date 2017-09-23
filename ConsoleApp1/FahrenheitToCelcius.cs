2eb2ef0129dc90a75649ab20b8c20bfb1bc4b7c5
nsmotritski <nsmotritski@gmail.com>
2017-09-23 10:58:45 +0300

    FahrenheitToCelcius done

---
+++ ConsoleApp1/FahrenheitToCelcius.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class FahrenheitToCelcius
+    {
+        static void Main()
+        {
+            /*
+             * добиться запуска этой программы в выбранной IDE или скомпилировать и запустить её при помощи компилятора командной строки.
+             * изменить программу, чтобы она переводила градусы из шкалы Фаренгейта в шкалу Цельсия.
+             * Для перевода температуры из шкалы Фаренгейта в шкалу Цельсия нужно от исходного числа отнять 32 и умножить результат на 5/9. 
+             */
+            Console.Write("Input Fahrenheit: ");
+            string s = Console.ReadLine();
+            double degrees = double.Parse(s);
+            Console.Write("In Celcius: ");
+            Console.WriteLine((degrees - 32) * 5 / 9);
+            Console.ReadKey();
+        }
+    }
+
+}
